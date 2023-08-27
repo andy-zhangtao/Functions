1687093c0d111e879c426c1141db0f84a8888ba2
andy <ztao8607@gmail.com>
2023-08-27 21:23:01 +0800

    add weaviate create api
    #### [New Feature]: add weaviate create api

---
+++ api/var.go
@@ -0,0 +1,16 @@
+package handler
+
+const (
+	RequestVersionDefault = RequestVersionV1
+	RequestVersionV1      = "v1"
+)
+
+const (
+	EnvWeaviateHost   = "WEAVIATE_HOST"
+	EnvWeaviateSchema = "WEAVIATE_SCHEMA"
+	EnvWewaviateKey   = "WEAVIATE_KEY"
+)
+
+const (
+	DiaryClassName = "Diary"
+)
