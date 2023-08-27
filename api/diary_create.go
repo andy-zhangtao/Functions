07ea94a8416476a198ab86ddf687b4001b160889
andy <ztao8607@gmail.com>
2023-08-27 21:32:09 +0800

    Change code layout

--- api/diary_create.go
+++ api/diary_create.go
@@ -1,13 +1,67 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/sirupsen/logrus"
+	"github.com/weaviate/weaviate-go-client/v4/weaviate"
+	"github.com/weaviate/weaviate-go-client/v4/weaviate/auth"
 )
+
+const (
+	RequestVersionDefault = RequestVersionV1
+	RequestVersionV1      = "v1"
+)
+
+const (
+	EnvWeaviateHost   = "WEAVIATE_HOST"
+	EnvWeaviateSchema = "WEAVIATE_SCHEMA"
+	EnvWewaviateKey   = "WEAVIATE_KEY"
+)
+
+const (
+	DiaryClassName = "Diary"
+)
+
+type weaviateClient struct {
+	client *weaviate.Client
+}
+
+func NewWeaviateClient(host, schema, key string) (*weaviateClient, error) {
+	cfg := weaviate.Config{
+		Host:       "localhost:8080",
+		Scheme:     "http",
+		AuthConfig: auth.ApiKey{Value: key},
+	}
+	client, err := weaviate.NewClient(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("could not create weaviate client: %v", err)
+	}
+
+	return &weaviateClient{
+		client: client,
+	}, nil
+}
+
+func (wc *weaviateClient) AddNewRecord(class string, properties map[string]string) error {
+	data := make(map[string]string)
+
+	for key, val := range properties {
+		data[key] = val
+	}
+
+	created, err := wc.client.Data().Creator().WithClassName(class).WithProperties(data).Do(context.Background())
+	if err != nil {
+		return fmt.Errorf("could not create record: %v", err)
+	}
+
+	logrus.Infof("Created record with id [%+v]", created)
+	return nil
+}
 
 type DirayCreateModel struct {
 	User    string      `json:"user"`
